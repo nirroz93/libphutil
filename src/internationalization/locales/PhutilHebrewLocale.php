8091c9c4ecb1be5371b346170d5d3ca8f5198a87
Nir Rosenthal <nirroz93@gmail.com>
2017-11-14 12:42:08 +0000

    Adding heberw file based on the US english one

---
+++ src/internationalization/locales/PhutilHebrewLocale.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * The hebrew locale.
+ */
+final class PhutilHebrewLocale extends PhutilLocale {
+
+  public function getLocaleCode() {
+    return 'he_IL';
+  }
+
+  public function getLocaleName() {
+    return pht('Hebrew');
+  }
+
+}
